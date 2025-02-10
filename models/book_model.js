f0be4fc18c3247253a679c27474eaf836b39839c
Pekka Alaluukas <pekka.alaluukas@oamk.fi>
2025-02-10 10:33:12 +0200

    book aloitettu

--- models/book_model.js
+++ models/book_model.js
@@ -1,5 +1,12 @@
 const book ={
-
+    bookData:[
+        {'name' :"C++", 'author' :"Jim Smith"},
+        {'name' :"Java", 'author' :"Lisa Jones"},
+        {'name' :"MySQL", 'author' :"Bob Danieles"}
+    ],
+    getAllBooks(callback){
+        callback(this.bookData);
+    }
 }
 
 module.exports=book;
--- routes/book.js
+++ routes/book.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const book = require('../models/book_model');
 
 router.get('/',function(request, response){
-    response.send("TESTI");
+    book.getAllBooks(function(result){
+        response.json(result);
+    })
 });
 router.get('/:id');
 router.post('/');
 router.put('/:id');
 router.delete('/:id');
 
-module.exports=router
+module.exports=router;
